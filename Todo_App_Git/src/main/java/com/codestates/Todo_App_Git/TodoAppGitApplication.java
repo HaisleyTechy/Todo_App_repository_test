546fce2a477c9e9467f232e16c04014bfb05659a
happiness-ham <gpals276@gmail.com>
2022-10-21 17:48:36 +0900

    new project

---
+++ Todo_App_Git/src/main/java/com/codestates/Todo_App_Git/TodoAppGitApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.Todo_App_Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppGitApplication.class, args);
+	}
+
+}
---
+++ Todo_App_Git/src/test/java/com/codestates/Todo_App_Git/TodoAppGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.Todo_App_Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
